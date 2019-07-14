c01b26ef6184e96ead1cc0a998b26267c926467b
Thomas Lewis <TomPaul_67@yahoo.com>
2019-07-14 15:19:07 -0400

    added Axios component

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 var inquirer    = require("inquirer");
 var keys        = require("./keys.js");
 var Spotify     = require("node-spotify-api");
+var axios       = require("axios");
 
 var log         = console.log;
 //console.log(keys);
@@ -128,3 +129,34 @@ function searchSpotify(searchStr) {
 
 // 
 //npm i -g inspect-process
+
+
+
+
+
+// Run the axios.get function...
+// The axios.get function takes in a URL and returns a promise (just like $.ajax)
+axios
+  .get("https://en.wikipedia.org/wiki/Kudos_(granola_bar)")
+  .then(function(response) {
+    // If the axios was successful...
+    // Then log the body from the site!
+    console.log(response.data);
+  })
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });
